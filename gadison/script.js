40c0163518bf4dbe913d14b48a5f57a5d3ca5489
lesalonmaloi <gadsonnemaloi@gmail.com>
2022-10-14 09:14:39 +0300

    modified css

--- gadison/script.js
+++ gadison/script.js
@@ -1,7 +1,7 @@
 const smallCups = document.querySelectorAll('.cup-small')
 const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
-const remained = document.getElementById('remained')
+
 
 updateBigCup()
 
